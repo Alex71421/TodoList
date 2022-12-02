7c6447e1e93bfe9f818e7d52e2204a948fc40d3e
Milk-Gang123 <alexushveridze@mail.ru>
2022-12-02 22:10:10 +0300

    Implemented TaskViewModel and added observes to MainActivity

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+        }
+
+        taskViewModel.name.observe(this) {
+            binding.taskName.text = String.format("Task name: %s", it)
+        }
+        taskViewModel.description.observe(this) {
+            binding.taskName.text = String.format("Task description: %s", it)
+        }
     }
 }
